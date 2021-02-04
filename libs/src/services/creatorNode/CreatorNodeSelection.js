a07344331c6d122017c2d0006defa01985b47048
Raymond Jacobson <ray@audius.co>
2021-02-03 18:04:33 -0800

    [AUD-143] Clean up pass/blocklist interface and record monitoring cbs

--- libs/src/services/creatorNode/CreatorNodeSelection.js
+++ libs/src/services/creatorNode/CreatorNodeSelection.js
@@ -7,8 +7,6 @@ class CreatorNodeSelection extends ServiceSelection {
     creatorNode,
     numberOfNodes,
     ethContracts,
-    whitelist,
-    blacklist,
     timeout = null
   }) {
     super({
@@ -17,8 +15,8 @@ class CreatorNodeSelection extends ServiceSelection {
         const services = await this.ethContracts.getServiceProviderList(CREATOR_NODE_SERVICE_NAME)
         return services.map(e => e.endpoint)
       },
-      whitelist,
-      blacklist
+      whitelist: creatorNode.passList,
+      blacklist: creatorNode.blockList
     })
     this.creatorNode = creatorNode
     this.numberOfNodes = numberOfNodes
@@ -208,6 +206,30 @@ class CreatorNodeSelection extends ServiceSelection {
       servicesMap[service.request.id] = service.response.data
       return service.request.id
     })
+
+    // Record metrics
+    if (this.creatorNode.monitoringCallbacks.healthCheck) {
+      healthCheckedServices.forEach(check => {
+        const url = new URL(check.request.url)
+        const data = check.response.data.data
+        this.creatorNode.monitoringCallbacks.healthCheck({
+          endpoint: url.origin,
+          pathname: url.pathname,
+          version: data.version,
+          git: data.git,
+          selectedDiscoveryNode: data.selectedDiscoveryProvider,
+          databaseSize: data.databaseSize,
+          totalMemory: data.totalMemory,
+          usedMemory: data.usedMemory,
+          totalStorage: data.storagePathSize,
+          usedStorage: data.storagePathUsed,
+          maxFileDescriptors: data.maxFileDescriptors,
+          allocatedFileDescriptors: data.allocatedFileDescriptors,
+          receivedBytesPerSec: data.receivedBytesPerSec,
+          transferredBytesPerSec: data.transferredBytesPerSec
+        })
+      })
+    }
 
     return { healthyServicesList, healthyServicesMap: servicesMap }
   }
